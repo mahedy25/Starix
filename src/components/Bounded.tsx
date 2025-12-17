ac9f7b0bc70fe7f4ebc6f4677ebb17660419cd86
mahedy25 <mahedyhasan873@gmail.com>
2025-12-17 23:23:07 +0600

    fixed bounded error

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -7,19 +7,19 @@ type BoundedProps = {
 }
 
 export const Bounded = ({
-  as: Comp = 'section',
+
   className,
   children,
   ...restProps
 }: BoundedProps) => {
   return (
-    <Comp
+    <section
       className={clsx('px-4 first:pt-10 md:px-6', className)}
       {...restProps}
     >
       <div className='mx-auto flex w-full max-w-7xl flex-col items-center'>
         {children}
       </div>
-    </Comp>
+    </section>
   )
 }
